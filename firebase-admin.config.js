6459004d9adae66275edc0d828e068a4d6eed969
MrVoidLink <mr.voidlink@gmail.com>
2025-08-06 14:32:18 +0330

    fix: secure firebase config and remove sensitive data

--- firebase-admin.config.js
+++ firebase-admin.config.js
@@ -1,14 +1,9 @@
-// firebase-admin.config.js
 import admin from "firebase-admin";
 
 const serviceAccount = {
-  "type": "service_account",
-  "project_id": "your-project-id",
-  "private_key_id": "xxx",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nxxx\n-----END PRIVATE KEY-----\n",
-  "client_email": "xxx@xxx.iam.gserviceaccount.com",
-  "client_id": "xxx",
-  ...
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 
 if (!admin.apps.length) {
