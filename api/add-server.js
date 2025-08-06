4926a7085b36df13ce32e3edb6317da1330aa8cc
MrVoidLink <mr.voidlink@gmail.com>
2025-08-06 23:20:59 +0330

    Fix import path in add-server API to use ../lib/firebase-admin and add error logging

--- api/add-server.js
+++ api/add-server.js
@@ -2,7 +2,6 @@ import { db } from "../lib/firebase-admin";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
-
     return res.status(405).json({ message: "Method not allowed" });
   }
 
