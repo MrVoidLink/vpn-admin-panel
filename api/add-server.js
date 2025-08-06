8ac1c012a2608e0e96d76eb5909f33a5fb074842
MrVoidLink <mr.voidlink@gmail.com>
2025-08-06 22:40:15 +0330

    Fix import path for Firebase Admin SDK in add-server API

--- api/add-server.js
+++ api/add-server.js
@@ -1,4 +1,4 @@
-import { db } from "../../util/firebase-admin"; // مسیر رو با توجه به پروژه‌ات تنظیم کن
+import { db } from "../src/utils/firebase-admin"; // مسیر رو با توجه به پروژه‌ات تنظیم کن
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
